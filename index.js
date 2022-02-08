80c1c5233fc43b6bd4939ef90bd39cb5f560d519
Rizal Ardhi Rahmadani <reyzeal@users.noreply.github.com>
2022-02-08 22:32:37 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const mustacheExpress = require('mustache-express');
 const engine = require("./engine")
+const axios = require("axios") 
 const app = express()
 
 app.engine('mustache', mustacheExpress());
@@ -9,10 +10,11 @@ app.set('view engine', 'mustache');
 
 app.get("/", async (req,res) => {
     let data = await engine()
-    let payload = {}
-    for(let i=1;i<=5;i++){
-        payload["data"+i] = data[i-1]
-    }
+    //let payload = {}
+    //for(let i=1;i<=5;i++){
+    //    payload["data"+i] = data[i-1]
+    //}
+    const payload = await axios.get(`https://katla.vercel.app/api/define/${data}`).then(r => r.data)
     res.render("index", payload)
 })
 
